a2e129a42e2bc4e825ad1ac62ac3131b3c90774c
far1h <farihmuhammad45@gmail.com>
2024-09-22 22:15:47 +0700

    Add sign up functionality

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -22,6 +23,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signUpClicked(_ sender: UIButton) {
+        
+        if let email = emailText.text, !email.isEmpty,
+               let password = passwordText.text, !password.isEmpty {
+            Auth.auth().createUser(withEmail: email, password: password) { authdata, error in
+                if let errorMessage = error {
+                    self.makeAlert(titleInput: "Error", messageInput: errorMessage.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+        }
+    }
+    
+    func makeAlert(titleInput:String, messageInput:String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
 }
 
